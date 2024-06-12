d79425a6ca1d639300c28b264a1c640dabf847bc
bappi4381 <sajjadul.islam@egeneration.co>
2024-06-12 17:22:40 +0600

    project details

--- resources/views/front/details/details.blade.php
+++ resources/views/front/details/details.blade.php
@@ -7,9 +7,10 @@
     <!-- Blog Post -->
     @if ($project)
     <div class="blog-post">
-        <img src="{{asset('storage/projects/'.$project->image)}}" alt="Post Image" style="width: 80%; max-height: 450px;">
         <h1 class="mt-4">{{ $project->project_title }}</h1> <!-- Assuming 'project_title' is a field in your Project model -->
         <p class="text-muted">Posted on {{ $project->created_at->format('F j, Y') }}</p> <!-- Assuming 'created_at' is a timestamp field -->
+        <img src="{{asset('storage/projects/'.$project->image)}}" alt="Post Image" style="width: 80%; max-height: 450px;">
+        
         <p>{!! $project->project_description !!}</p>
     </div>
     @else
--- resources/views/front/index/index.blade.php
+++ resources/views/front/index/index.blade.php
@@ -55,7 +55,7 @@
         <div class="col-md-6 col-lg-3">
             <div class="box">
               <div class="img-box">
-                <img src="{{asset('storage/service/'.$service->service_image)}}" alt="">
+                <img src="{{asset('storage/service/'.$service->service_image)}}" alt="" style="width:100%; max-height: 250px !important;">
               </div>
               <div class="detail-box">
                 <h5>
